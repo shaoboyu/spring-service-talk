456f96e203c4e246e1ee2aed404beb6b038ebb76
于绍博 <944050595@qq.com>
2017-12-29 20:41:38 +0800

    毕业设计项目初始化

--- src/main/java/com/feinno/talk/springservicetalk/HelloController.java
+++ src/main/java/com/feinno/talk/springservicetalk/HelloController.java
@@ -1,19 +1,15 @@
 package com.feinno.talk.springservicetalk;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @Autowired
-    private TalkProperties talkProperties;
-
     @RequestMapping(value = "/hello" ,method = RequestMethod.GET)
-    public String say(){
-        return "Hello SpringBoot"+talkProperties.getAge()+talkProperties.getCupSize();
+
+    public String hello(){
+        return "index";
     }
 }
--- src/main/java/com/feinno/talk/springservicetalk/SpringServiceTalkApplication.java
+++ src/main/java/com/feinno/talk/springservicetalk/SpringServiceTalkApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringServiceTalkApplication {
 
+	/**
+	* 主程序入口
+	*@param:
+	*@return:
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringServiceTalkApplication.class, args);
+		System.out.println("==============Spring-Service-Talk Start Finish============");
 	}
 }
