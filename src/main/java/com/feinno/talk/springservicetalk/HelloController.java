60464303d9a4e2288c3c1ebbdcd1b9dc2f80c81c
于绍博 <944050595@qq.com>
2017-12-30 19:43:00 +0800

    服务日志管理

--- src/main/java/com/feinno/talk/springservicetalk/HelloController.java
+++ src/main/java/com/feinno/talk/springservicetalk/HelloController.java
@@ -1,5 +1,7 @@
 package com.feinno.talk.springservicetalk;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloController {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping(value = "/hello" ,method = RequestMethod.GET)
 
     public String hello(){
+        LOGGER.info("HelloController start");
         return "index";
     }
 }
