487a396ea9a1f5b4124af5835c129bade948e762
yushaobo <yusahobo@feinno.com>
2018-02-03 13:59:11 +0800

    微服务打包修正conf

--- src/main/resources/bin/run.sh
+++ src/main/resources/bin/run.sh
@@ -1,12 +1,29 @@
-###启动
+#!/bin/bash
+DEPLOY_DIR=`pwd`
+echo "DEPLOY_DIR=$DEPLOY_DIR"
+PIDS=`ps -ef | grep java | grep $DEPLOY_DIR |awk '{print $2}'`
+if [ -n "$PIDS" ]; then
+    echo "ERROR: The beast-member already started!"
+    echo "PID: $PIDS"
+    exit 1
+fi
 
-#!/bin/sh
+LIB_DIR=$DEPLOY_DIR/lib
+echo "lib dir: $LIB_DIR"
+MAIN_JARS=`ls $LIB_DIR|grep beast|grep main|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" " "`
+echo "main jar: $MAIN_JARS"
+nohup java -Xms128m -Xmx1024m -XX:MaxPermSize=64M -jar $MAIN_JARS -console 2>/dev/null >/dev/null &
 
-moduleName="ps"
-pidPath="/var/run/$moduleName-tpid"
+COUNT=0
+while [ $COUNT -lt 1 ]; do
+    echo -e ".\c"
+    sleep 1
+    COUNT=`ps -f | grep java | grep "$DEPLOY_DIR" | awk '{print $2}' | wc -l`
+    if [ $COUNT -gt 0 ]; then
+        break
+    fi
+done
 
-rm -f $pidPath
-
-nohup java -jar ./$moduleName.jar -server -Xms1024m -Xmx2048m -Xss256k > ./run.log 2>&1 &
-
-echo $! > $pidPath
+echo "OK!"
+PIDS=`ps -f | grep java | grep "$DEPLOY_DIR" | awk '{print $2}'`
+echo "PID: $PIDS"
